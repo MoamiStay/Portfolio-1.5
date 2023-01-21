c02343d6966f1357384b7f04ad098fb14c50ba03
Regine <reginenaas@gmail.com>
2023-01-22 00:24:02 +0100

    Added all html except.. (+some styling)
    .. except some img for dark-mode figures.
    Also started adding styling for nav bar and first section. not quite complete yet but it is a start.

--- portfolio-1.5/tailwind.config.cjs
+++ portfolio-1.5/tailwind.config.cjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
+  mode: "jit",
+  purge: ["./index.html"],
+  darkMode: "media",
   theme: {
     screens: {
       sm: "480px",
@@ -14,8 +17,8 @@ module.exports = {
         bg_dark: "#2C2C34",
         light_secondary: "#8FDFD1",
         dark_secondary: "#6A68DE",
-        light_text: "#6B222F",
-        dark_text: "#F2F2F2",
+        dark_text: "#6B222F",
+        light_text: "#F2F2F2",
         light_text_bg: "#FFE8EC",
         dark_text_bg: "#393949",
         white: "#FFFFFF",
