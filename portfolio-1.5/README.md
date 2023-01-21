eced99c2ad28e5bafdcc84d436a397181a1cc101
Regine <reginenaas@gmail.com>
2023-01-21 01:23:38 +0100

    Html-1
    Added nav and home html + 2 images for deco

--- portfolio-1.5/README.md
+++ portfolio-1.5/README.md
@@ -1,2 +1,8 @@
 # Portfolio-1.5
+
 Portfolio made with vanilla JS, HTML and TAILWIND-CSS
+
+WCAG color contrast:
+https://webaim.org/resources/contrastchecker/
+
+Tailwind: google chrome extension
--- portfolio-1.5/tailwind.config.cjs
+++ portfolio-1.5/tailwind.config.cjs
@@ -2,7 +2,25 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    screens: {
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1449px",
+    },
+    extend: {
+      colors: {
+        bg_light: "#F8CCD4",
+        bg_dark: "#2C2C34",
+        light_secondary: "#8FDFD1",
+        dark_secondary: "#6A68DE",
+        light_text: "#6B222F",
+        dark_text: "#F2F2F2",
+        light_text_bg: "#FFE8EC",
+        dark_text_bg: "#393949",
+        white: "#FFFFFF",
+      },
+    },
   },
   plugins: [],
-}
+};
