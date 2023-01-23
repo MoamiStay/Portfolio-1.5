c751ff731e056e92c883447995ff66bba75cf382
Regine <reginenaas@gmail.com>
2023-01-23 22:38:25 +0100

    Final adjustments before delivery
    Added menu function for mobile
    minor css alignments
    rewriting some text content

--- portfolio-1.5/src/main.js
+++ portfolio-1.5/src/main.js
@@ -1,12 +1,44 @@
+const navBar = document.querySelector("#nav-bar");
+const navLinks = document.querySelector("#nav-links");
+const menuDark = document.querySelector("#menu-dark");
+const menuLight = document.querySelector("#menu-light");
+const exitDark = document.querySelector("#X-dark");
+const exitLight = document.querySelector("#X-light");
+
+menuDark.addEventListener("click", showMenu);
+menuLight.addEventListener("click", showMenu);
+
+exitDark.addEventListener("click", hideMenu);
+exitLight.addEventListener("click", hideMenu);
+navBar.addEventListener("click", hideMenu);
+
+function showMenu() {
+  navBar.classList.remove("sm:hidden");
+  navLinks.classList.remove("sm:hidden");
+  menuDark.classList.remove("dark:flex");
+  menuLight.classList.add("hidden");
+  exitDark.classList.add("dark:flex");
+  exitLight.classList.remove("hidden");
+  exitLight.classList.add("flex");
+}
+
+function hideMenu() {
+  navBar.classList.add("sm:hidden");
+  navLinks.classList.add("sm:hidden");
+  menuDark.classList.add("dark:flex");
+  menuLight.classList.remove("hidden");
+  exitDark.classList.remove("dark:flex");
+  exitLight.classList.add("hidden");
+  exitLight.classList.remove("flex");
+}
+
 // NETLIFY/DOMENE
 // README
-// RESPONSIVENESS
-// MENU
 // CURRENT "PAGE" TRACKER
 // PORTFOLIOPAGES DROPDOWN INFO ON HOVER
 // MOBILE CAROUSEL/SWIPE PORTFOLIO
-// FIX BACK-TO-TOP BUTTON DESIGN
-// SHADOW COLOR ON "DESIGN"
+// FIX BACK-TO-TOP BUTTON DESIGN TO BE MORE VISIBLE
+// FIX SHADOW COLOR ON "DESIGN"
 // ACHOR LINKS WORK MORE PROPERLY
 // MENU SMOOTH SCROLL BETWEEN ANCHORS
 // ANIMATIONS
--- portfolio-1.5/tailwind.config.cjs
+++ portfolio-1.5/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./src/main.js"],
   mode: "jit",
   purge: ["./index.html"],
   darkMode: "media",
